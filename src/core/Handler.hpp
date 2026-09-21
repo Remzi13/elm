4e6d6050822e06cf6a150ca82f16e73bbdfea2eb
Remzi13 <remiznik@hotmail.com>
2026-09-21 20:00:44 +0200

    add core Handler

---
+++ src/core/Handler.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "core/Std.hpp"
+
+namespace elm {
+namespace core {
+
+    class Handler final {
+    public:
+        enum Type {
+            Core,
+            Resource,
+            Render,
+            None
+        };
+
+    public:
+        Handler() = default;
+
+        Handler(int value, Type type)
+            : m_type(type)
+            , m_value(value)
+        {
+        }
+
+        [[nodiscard]] constexpr bool IsValid() const noexcept { return m_value >= 0; }
+
+        constexpr bool operator==(const Handler&) const noexcept = default;
+        constexpr auto operator<=>(const Handler&) const noexcept = default;
+        
+        Type GetType() const { return m_type; }
+
+        int GetValue() const { return m_value; }
+
+    public:
+        int m_value { -1 };
+        Type m_type { Type::None };
+    };
+}
+}
+
+template <>
+struct std::hash<elm::core::Handler> {
+    [[nodiscard]] std::size_t operator()(elm::core::Handler handler) const noexcept
+    {
+        const auto index = static_cast<std::size_t>(handler.GetValue());
+        const auto type = static_cast<std::size_t>(handler.GetType());
+
+        return index ^ (type + 0x9e3779b9u + (index << 6u) + (index >> 2u));
+    }
+};
