42a69b1ad92ffb5ba66bea755a943a32a75e691a
Remzi13 <remiznik@hotmail.com>
2026-09-22 22:48:48 +0200

    fix for linux debug

--- src/core/Debug.hpp
+++ src/core/Debug.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#if defined(_MSC_VER)
 #define ELM_DEBUG_BREAK() __debugbreak()
+#else
+#define ELM_DEBUG_BREAK() asm volatile("int $3");
+#endif
 
 #define ELM_ASSERT(cond)                                         \
     do {                                                         \
